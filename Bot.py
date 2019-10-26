55089ce1124f793d74fc03c5fff36fc074718381
str1kebeam <53403100+str1kebeam@users.noreply.github.com>
2019-10-26 12:46:57 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -12,10 +12,5 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 
-reddit = praw.Reddit(client_id='acrtj7jRqKQlSw',
-                     client_secret='3dOyTX5ijxMYhgca02T32Ptt-y0',
-                     user_agent='<console:new_bot:0.0.1 (by /u/str1kebeam)>',
-                     username='str1kebeam',
-                     password='stg90shredder')
 r1 = reddit.redditor('str1kebeam')
 r2 = reddit.redditor('sidewind864')
